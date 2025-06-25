8ab5ed1b794bf2e07cc13408789fcfd43db0e1a9
Thanawat Petchsri <thanawat.petchsri@gmail.com>
2025-06-25 23:45:15 +0700

    feat: add watch, rename theme folder

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,21 @@
 import scss from 'rollup-plugin-scss'
 import { terser } from 'rollup-plugin-terser';
+import postcss from 'postcss';
+import autoprefixer from 'autoprefixer';
 
 export default {
-  input: './theme-src/js/index.js',
+  input: './src/js/index.js',
   output: {
     file: './theme/assets/script.min.js',
     format: 'iife',
   },
   plugins: [
-    scss({ fileName: 'style.min.css', outputStyle: 'compressed' }),
+    scss({
+      fileName: 'style.min.css',
+      outputStyle: 'compressed',
+      processor: () => postcss([autoprefixer()]),
+      watch: ['./src/scss']
+    }),
     terser()
   ]
 };
