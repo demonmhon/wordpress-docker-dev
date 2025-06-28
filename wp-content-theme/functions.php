535bfd5df205d05ffcd7eebc0df261519d5207dc
Thanawat Petchsri <thanawat.petchsri@gmail.com>
2025-06-28 15:16:38 +0700

    fix: uploads folder

--- wp-content-theme/functions.php
+++ wp-content-theme/functions.php
@@ -7,7 +7,6 @@ function register_navwalker(){
 	require_once get_template_directory() . '/inc/class-wp-bootstrap-navwalker.php';
 }
 add_action('after_setup_theme', 'register_navwalker');
-
 add_action('after_setup_theme', function() {
   add_theme_support('title-tag');
   add_theme_support('post-thumbnails');
